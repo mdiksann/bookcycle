81a32f86d5c556f7f93b88a94f64479360304fda
iksan <mdiksann@gmail.com>
2025-12-08 16:41:46 +0700

    refactor: update exchange routes to use authRequired middleware and controller imports

--- server/src/routes/exchange.routes.js
+++ server/src/routes/exchange.routes.js
@@ -1,9 +1,14 @@
-import { Router } from "express";
-import auth from "../middlewares/auth.js";
-import { createExchange } from "../controllers/exchange.controller.js";
+import { Router } from 'express';
+import { authRequired } from '../middlewares/auth.js';
+import * as ctrl from '../controllers/exchange.controller.js';
 
 const router = Router();
 
-router.post("/", auth, createExchange);
+router.post('/', authRequired, ctrl.createExchange); 
+router.get('/incoming', authRequired, ctrl.getIncoming); 
+router.get('/outgoing', authRequired, ctrl.getOutgoing); 
+router.patch('/:id/accept', authRequired, ctrl.accept);
+router.patch('/:id/reject', authRequired, ctrl.reject);
+router.patch('/:id/cancel', authRequired, ctrl.cancel);
 
 export default router;
